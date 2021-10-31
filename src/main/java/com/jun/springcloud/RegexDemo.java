5577dd4156881dd99779028b71664ecee28cc508
Junhyung Ki <kijoonh91@gmail.com>
2021-10-31 21:01:17 +0100

    regex syntax you looking for

--- src/main/java/com/jun/springcloud/RegexDemo.java
+++ src/main/java/com/jun/springcloud/RegexDemo.java
@@ -6,18 +6,17 @@ import java.util.regex.Pattern;
 public class RegexDemo {
 
     public void run() {
-        //TODO: regex requirement would be:
-        // no alphabets right before and after the match but you should allow
-        // special characters like: . , ! ? etc
-        // no alaph
-        // "(^|\W)(Urlaub)(?![a-zA-Z])"
-
         System.out.println("----- Regex Test Start -----");
-        //String rgx = "(Urlaub)+([ .,?!-])?";
-        String rgx = "(?<=(^|\\W))(Urlaub)(?=[ .,?!-])";
+        String rgxLB = "(?<![A-z|À-ÿ|-])(";
+        String rgxLA = ")(?![A-z|À-ÿ|-])";
+
+        StringBuilder stringBuilder = new StringBuilder("adfs".length());
+        String rgx = stringBuilder.append(rgxLB).append("Urlaub").append(rgxLA).toString();
         String exText = "The fat cat ran down the street.\n" +
-                "It was searching for a mouse to eat.\n" +
-                "Urlaub, Urlaub! Urlaub ist toll als Urlaubszeit fat fat bat cat";
+                "It üUrlaub bUrlaub urlaubt urlaub3 CCUrlaub -urlaub _urlaub !urlaub .urlaub   urlaub~  \n" +
+                "urlaub::  was searching for a mouse to eat.\n" +
+                "Urlaubá Urlaub- Urlaubß, Urlaub Urlaubä Urlauba! Urlaubü urlaub?!! " +
+                "urlaub_ urlaub- urlaub. urlaub, ist toll als Urlaubszeit fat fat bat cat urlaub\n";
         Pattern pattern = Pattern.compile(rgx, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(exText);
         String resultText = matcher.replaceAll("TEST");
